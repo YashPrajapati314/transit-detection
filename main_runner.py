b659df1a183024314dbeae7a021abe63f1c5156d
Yash Prajapati <yashprajapati271828@gmail.com>
2025-04-27 13:33:20 +0530

    Write True

--- main_runner.py
+++ main_runner.py
@@ -3,5 +3,5 @@ from main_star_data_computer import compute_star_data
 for id in range(1001, 1100):
     star_name = f'OGLE-TR-{id}'
 
-    compute_star_data(star_name, compute_for_full_curve=True, write=False)
-    compute_star_data(star_name, compute_for_full_curve=False, write=False)
+    compute_star_data(star_name, compute_for_full_curve=True, write=True)
+    compute_star_data(star_name, compute_for_full_curve=False, write=True)
