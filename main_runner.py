cad40f9e419b828e8f44fd95d91246973470d6c6
Yash Prajapati <yashprajapati271828@gmail.com>
2025-04-27 13:42:40 +0530

    Main function format

--- main_runner.py
+++ main_runner.py
@@ -2,6 +2,11 @@ from main_star_data_computer import compute_star_data
 
 for id in range(1001, 1100):
     star_name = f'OGLE-TR-{id}'
-
+    
+    print(f'\n{star_name}\n')
+    
+    print(f'\nFull Curve\n')
     compute_star_data(star_name, compute_for_full_curve=True, write=True)
+    
+    print(f'\nt14 Region\n')
     compute_star_data(star_name, compute_for_full_curve=False, write=True)
